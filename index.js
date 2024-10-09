95e24cb45709a40514488774d03109a740df0f69
Carlos Osmar <carlososmarof@gmail.com>
2024-10-09 16:07:00 -0400

    post endpoint

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const app = express();
 const port = 3000;
 
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',  
-    password: '',
-    database: 'devops_crud'
-});
-
-db.connect(err => {
-    if (err) {
-        console.error('Error conectando a la base de datos:', err);
-        return;
-    }
-    console.log('Conectado a la base de datos MySQL');
-});
-
-
 app.use(express.json());
+
+
+app.post('/items', (req, res) => {
+    const { name, description, price } = req.body;
+    const query = 'INSERT INTO items (name, description, price) VALUES (?, ?, ?)';
+    db.query(query, [name, description, price], (err, result) => {
+        if (err) {
+            console.error('Error insertando el item:', err);
+            return res.status(500).send('Error al crear el item');
+        }
+        res.status(201).send({ id: result.insertId, name, description, price });
+    });
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
